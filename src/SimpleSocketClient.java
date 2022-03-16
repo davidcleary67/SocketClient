32ca61920c82dd5204333652a5b97836bd53bfe0
David Cleary <davidcgcleary@gmail.com>
2022-03-16 12:24:26 +1100

    Added comments

--- src/SimpleSocketClient.java
+++ src/SimpleSocketClient.java
@@ -3,49 +3,57 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 
-public class SimpleSocketClient
-{
-    public static void main( String[] args )
-    {
-        if( args.length < 2 )
-        {
-            System.out.println( "Usage: SimpleSocketClient <server> <port>" );
-            System.exit( 0 );
+/**
+ * Simple socket client that sends messages via a socket on an output stream
+ * and receives a response back on an input stream.
+ * The socket host and port are specified by the user from the command line.
+ */
+public class SimpleSocketClient {
+	
+    /**
+     * Main function.
+     * Get server and port from command line argument and connect to the server via the associated socket.
+     * @param args - server and port for communication
+     */
+    public static void main(String[] args) {
+        if(args.length < 2) {
+            System.out.println("Usage: SimpleSocketClient <server> <port>");
+            System.exit(0);
         }
-        String server = args[ 0 ];
+        
+        String server = args[0];
         int port = Integer.parseInt(args[1]);
 
-        System.out.println( "Loading contents of URL: " + server );
+        System.out.println("Connect to server at: " + server + " using port: " + port);
 
         try
         {
             // Connect to the server
-            Socket socket = new Socket( server, port );
+            Socket socket = new Socket(server, port);
 
             // Create input and output streams to read from and write to the server
-            PrintStream out = new PrintStream( socket.getOutputStream() );
-            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
+            PrintStream out = new PrintStream(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            // Follow the HTTP protocol of GET <path> HTTP/1.0 followed by an empty line
-            out.println( "GetCreditStatus" );
-            out.println( "GetAppraisal" );
+            // Send messages to server
+            out.println("GetCreditStatus");
+            // Blank message indicates the connection can be closed 
             out.println();
 
-            // Read data from the server until we finish reading the document
+            // Read and print out response from the server
+            // Blank line indicates the end of the response
             String line = in.readLine();
-            while( line != null )
-            {
-                System.out.println( line );
+            while(line != null) {
+                System.out.println(line);
                 line = in.readLine();
             }
 
-            // Close our streams
+            // Close streams and socket
             in.close();
             out.close();
             socket.close();
         }
-        catch( Exception e )
-        {
+        catch(Exception e) {
             e.printStackTrace();
         }
     }
